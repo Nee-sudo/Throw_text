5ba500e50dd14216fc84d99ab53804a116f6ebc8
AnthonyPint0 <antoniocruze17@gmail.com>
2025-02-25 14:59:57 +0530

    level 1 organizing

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const path = require('path');
-require('dotenv').config(); // Load environment variables from .env file
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+require("dotenv").config(); // Load environment variables from .env file
 const app = express();
 const PORT = 3000;
-const requestIp = require('request-ip');
+const requestIp = require("request-ip");
 
-const URI = process.env.MONGO_URI || 'mongodb+srv://neer:bjFBXFCYd00Gifiv@pdf-uploading-site.ges8oic.mongodb.net/?retryWrites=true&w=majority'; // MongoDB URI  
+const URI =
+  process.env.MONGO_URI ||
+  "mongodb+srv://neer:bjFBXFCYd00Gifiv@pdf-uploading-site.ges8oic.mongodb.net/?retryWrites=true&w=majority"; // MongoDB URI
 // Connect to MongoDB (replace 'your_database_url' with your actual MongoDB URL)
-mongoose.connect(URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => { 
-  console.log('Connected to MongoDB');
-}).catch(err => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(URI, {})
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Define a schema for your data
 const textSchema = new mongoose.Schema({
-  content: String
+  content: String,
 });
 
 // Create a model based on the schema
-const Text = mongoose.model('Text', textSchema);
+const Text = mongoose.model("Text", textSchema);
 
 // Define message schema
 const messageSchema = new mongoose.Schema({
@@ -39,66 +41,66 @@ const Message = mongoose.model("Message", messageSchema);
 app.use(bodyParser.json());
 app.use(requestIp.mw()); // Middleware to get IP address
 // Serve the HTML file
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // API endpoint to save text
-app.post('/api/saveText', async (req, res) => {
+app.post("/api/saveText", async (req, res) => {
   const content = req.body.content;
 
   try {
     // Create a new Text document and save it to the database
     const newText = new Text({ content });
     await newText.save();
-    res.status(200).send('Text saved successfully.');
+    res.status(200).send("Text saved successfully.");
   } catch (error) {
     console.error(error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
   }
 });
 
 // API endpoint to get all texts
-app.get('/api/getAllTexts', async (req, res) => {
+app.get("/api/getAllTexts", async (req, res) => {
   try {
     // Retrieve all Text documents from the database
     const allTexts = await Text.find();
     res.json(allTexts);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
   }
 });
 
-app.delete('/api/deleteText/:textId', async (req, res) => {
+app.delete("/api/deleteText/:textId", async (req, res) => {
   const { textId } = req.params;
 
   try {
     // Delete the text with the specified textId from the database
     await Text.findByIdAndDelete(textId);
-    res.status(200).send('Text deleted successfully.');
+    res.status(200).send("Text deleted successfully.");
   } catch (error) {
     console.error(error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
   }
 });
 // API endpoint to get a specific text by ID
-app.get('/api/getText/:textId', async (req, res) => {
+app.get("/api/getText/:textId", async (req, res) => {
   const { textId } = req.params;
-  console.log('Fetching text with ID:', textId);
+  console.log("Fetching text with ID:", textId);
 
   try {
     if (!mongoose.Types.ObjectId.isValid(textId)) {
-      return res.status(400).json({ error: 'Invalid ID format' });
+      return res.status(400).json({ error: "Invalid ID format" });
     }
 
     const text = await Text.findById(textId);
     if (!text) {
-      return res.status(404).json({ error: 'Text not found' });
+      return res.status(404).json({ error: "Text not found" });
     }
 
     res.json(text);
   } catch (error) {
-    console.error('Database error:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Database error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
@@ -106,16 +108,19 @@ app.get('/api/getText/:textId', async (req, res) => {
 const visitorSchema = new mongoose.Schema({
   ip: String,
   visits: { type: Number, default: 1 },
-  lastVisit: { type: Date, default: Date.now }
+  lastVisit: { type: Date, default: Date.now },
 });
 
-const Visitor = mongoose.model('Visitor', visitorSchema);
+const Visitor = mongoose.model("Visitor", visitorSchema);
 
 // Track Visitor
-app.get('/api/track-visitor', async (req, res) => {
+app.get("/api/track-visitor", async (req, res) => {
   try {
-    const ip = req.clientIp || req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    console.log('Visitor IP:', ip);
+    const ip =
+      req.clientIp ||
+      req.headers["x-forwarded-for"] ||
+      req.socket.remoteAddress;
+    console.log("Visitor IP:", ip);
 
     let visitor = await Visitor.findOne({ ip });
 
@@ -128,40 +133,40 @@ app.get('/api/track-visitor', async (req, res) => {
       await visitor.save();
     }
 
-    res.json({ message: 'Visitor tracked', visits: visitor.visits });
+    res.json({ message: "Visitor tracked", visits: visitor.visits });
   } catch (error) {
-    console.error('Error tracking visitor:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error tracking visitor:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 // Get Visitor Stats
-app.get('/api/visitor-stats', async (req, res) => {
+app.get("/api/visitor-stats", async (req, res) => {
   try {
     const uniqueVisitors = await Visitor.countDocuments();
     const frequentVisitors = await Visitor.find().sort({ visits: -1 }).limit(5); // Top 5 frequent visitors
 
     res.json({ uniqueVisitors, frequentVisitors });
   } catch (error) {
-    console.error('Error fetching visitor stats:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error fetching visitor stats:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
-//google route 
-app.get('/google3634443e1c428dc1.html', (req, res) => {
-  const filePath = path.join(__dirname, 'google3634443e1c428dc1.html'); // Path to a local PDF
-  res.setHeader('Content-Disposition', 'inline');
+//google route
+app.get("/google3634443e1c428dc1.html", (req, res) => {
+  const filePath = path.join(__dirname, "google3634443e1c428dc1.html"); // Path to a local PDF
+  res.setHeader("Content-Disposition", "inline");
   res.sendFile(filePath);
 });
 //sitemap
-app.get('/sitemap.xml', (req, res) => {
-  const filePath = path.join(__dirname, 'sitemap.xml'); 
+app.get("/sitemap.xml", (req, res) => {
+  const filePath = path.join(__dirname, "sitemap.xml");
   res.sendFile(filePath);
 });
 
 // to get the ocean.html file
-app.get('/ocean', (req, res) => {
-  const filePath = path.join(__dirname, 'public/ocean.html'); 
+app.get("/ocean", (req, res) => {
+  const filePath = path.join(__dirname, "public/ocean.html");
   res.sendFile(filePath);
 });
 // API endpoint to save messages
